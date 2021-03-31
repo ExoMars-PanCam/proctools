b053676d7eb10a7e5635cfe0bfc4f8aaa3076447
Ariel Ladegaard <legendin@gmail.com>
2021-03-31 20:32:44 +0100

    feat(core): add pre-init log buffering
    Ensure that any logging events that occur after import
    of `logger`but before its initialisation are buffered.
    This allows e.g. CLI exceptions to be logged
    appropriately, and lays the groundwork for implementing
    support for a fallback log file.
    
    Expose `init` log level and file mode parameters.
    
    BREAKING CHANGE: rename existing `init` parameters.

--- src/proctools/cli/logger.py
+++ src/proctools/cli/logger.py
@@ -1,66 +1,107 @@
 import logging
+import logging.handlers
 from pathlib import Path
-import signal
 import sys
 import time
+from typing import Optional
 
-# TODO: any need to call logging.shutdown() after exception or signal intercepts?
-#  The logging module hooks .shutdown() via atexit anyway and testing has revealed no
-#  issues with incomplete log files...
+initialised: bool = False  # (see python issue 34939 for why this needs to be at the top specifically)
 
 
-def init(log_path: Path = None, cli_output=True):
-    if not log_path and not cli_output:
+def init(file: Optional[Path] = None, stdout: bool = True, mode: str = "a", level: int = logging.INFO):
+    global buffer, initialised, root
+
+    if initialised:
+        root.warning("Attempting to reinitialise the log; ignoring")
+        return
+    elif file is None and not stdout:
+        # still commit critical entries to the buffer so they can be retrieved if needed during cleanup
         logging.disable(logging.CRITICAL)
         return
+    elif level not in (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL):
+        raise ValueError(f"'{level}' is not a valid log level")
 
-    root = logging.getLogger("")
-    root.setLevel(logging.INFO)
-
-    if cli_output:
-        fmt = "%(name)-26s %(levelname)-8s %(message)s"
+    if stdout:
+        fmt = "%(name)-18s %(levelname)-8s %(message)s"
         try:
             import coloredlogs
-            coloredlogs.install(level=logging.INFO, logger=root, fmt=fmt)
-
+            coloredlogs.install(level=level, logger=root, fmt=fmt)
         except ImportError:
-            cli = logging.StreamHandler()
-            # cli.setLevel(logging.INFO)
+            cli = logging.StreamHandler(sys.stdout)
+            cli.setLevel(level)
             cli_fmt = logging.Formatter(fmt)
             cli.setFormatter(cli_fmt)
             root.addHandler(cli)
 
-    if log_path is not None:
-        fh = logging.FileHandler(log_path, mode="w")
-        # fh.setLevel(logging.INFO)
+    if file is not None:
+        fh = logging.FileHandler(file, mode=mode)
+        fh.setLevel(level)
         logging.Formatter.converter = time.gmtime
-        fh_fmt = logging.Formatter(fmt="%(asctime)s.%(msecs)03d "
-                                       "%(name)-26s "
+        fh_fmt = logging.Formatter(fmt="%(asctime)s.%(msecs)03dZ "
+                                       "%(name)-18s "
                                        "%(levelname)-8s "
                                        "%(message)s",
                                    datefmt="%Y-%m-%dT%H:%M:%S")
         fh.setFormatter(fh_fmt)
         root.addHandler(fh)
 
-    # Log all uncaught exceptions encountered at runtime
-    def handle_exception(exc_type, exc_value, exc_traceback):
-        if issubclass(exc_type, KeyboardInterrupt):  # SIGINT
-            sys.__excepthook__(exc_type, exc_value, exc_traceback)
-            return
-        root.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
-        root.critical("Processing aborted")
+    # flush the temporary log record buffer to the new handler(s)
+    handlers = [h for h in root.handlers if h is not buffer]
+    buffer.set_targets(handlers)
+    buffer.close()
+    root.removeHandler(buffer)
 
-    sys.excepthook = handle_exception
+    initialised = True
 
-    # Log SIGINT (e.g. kill -2 / Ctrl+C) and SIGTERM (e.g. kill -15) events
-    def handle_signals(signum, frame):
-        signame = {
-            2: "SIGINT",
-            15: "SIGKILL",
-        }.get(signum, "UNKNOWN signal")
-        root.critical(f"{signame} received from the system")
-        root.critical("Processing aborted")
-        sys.exit(1)
 
-    signal.signal(signal.SIGINT, handle_signals)
-    signal.signal(signal.SIGTERM, handle_signals)
+def _handle_exception(exc_type, exc_value, exc_traceback):
+    """Log all uncaught exceptions encountered at runtime"""
+    global initialised, root
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    root.critical(f"Uncaught exception {exc_type.__name__}: {exc_value}",
+                  exc_info=(exc_type, exc_value, exc_traceback))
+    if not initialised:
+        # ensure pre-init exceptions are at least logged to stdout
+        # (in the future we should append to a default log file for these events)
+        init()
+    logging.shutdown()
+
+
+class _BufferHandler(logging.Handler):
+    def __init__(self, targets=None):
+        super().__init__()
+        self.targets = targets
+        self.buffer = []
+
+    def emit(self, record):
+        self.buffer.append(record)
+
+    def set_targets(self, targets):
+        self.targets = targets
+
+    def flush(self):
+        self.acquire()
+        try:
+            if self.targets:
+                for record in self.buffer:
+                    for target in self.targets:
+                        target.handle(record)
+                self.buffer = []
+        finally:
+            self.release()
+
+    def close(self):
+        try:
+            self.flush()
+        finally:
+            logging.Handler.close(self)
+
+
+# direct log entries to temporary buffer on import; subsequently redirected to proper handler(s) by `init`
+root = logging.getLogger("")
+root.setLevel(logging.DEBUG)
+buffer = _BufferHandler()
+root.addHandler(buffer)
+sys.excepthook = _handle_exception
