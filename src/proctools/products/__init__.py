18e79874b77e36a0b6f431c04333f5f0a3d6354d
Ariel Ladegaard <legendin@gmail.com>
2021-11-15 17:11:51 +0000

    feat(products): add Bayer pattern subframe helper
    BayerSlice transposes a Bayer pattern based on subframe offset
    parameters, and exposes a `slice` for each colour cell.

--- src/proctools/products/__init__.py
+++ src/proctools/products/__init__.py
@@ -2,6 +2,6 @@ from .adapters import KeyTable, MultiData
 from .dataproduct import DataProduct
 from .mixins import ApplicableCameraMixin, ObservationalMixin
 from .loader import ProductLoader as _Loader
-from .util import get_md5sum
+from .util import BayerSlice, get_md5sum
 
 loader = _Loader()
