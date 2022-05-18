f5ad7f358c4906ca0749c1e93f03945235c91398
Ariel Ladegaard <legendin@gmail.com>
2022-05-18 17:23:49 +0100

    feat(products): iterate depot interface and feats
    Increase utility of usage tracking feature by allowing enumerated
    states, e.g., not just "unused" vs "used", but introducing "loaded",
    "processed","failed", etc. This has the added benefit of simplifying the
    admittedly cryptic signature of `retrieve`. The members of the new
    Status enum are notfinal and may be made user-extendable in the future.
    
    Reimplement usage tracking internally to remove the need for
    synchronised product and usage map sorts and cross-lookups. Marginal
    performance gain, but code clarity is arguably up.
    
    Add (re-add?) usage summary generation.
    
    Prune `next` functionality; nice-to-have feature that was lacking an
    actual use case.

--- src/proctools/products/depot.py
+++ src/proctools/products/depot.py
@@ -1,26 +1,44 @@
 import logging
+import enum
 from collections import defaultdict
 from pathlib import Path
-from typing import Callable, Dict, List, Optional, Union
+from typing import Callable, Dict, List, Optional, Sequence, Union
 from xml.parsers.expat import ExpatError
 
 from . import DataProduct
 
 
 class ProductDepot:
-    """DataProduct directory loader, product type organiser and usage tracker.
+    """DataProduct directory loader, product type organiser and usage status tracker.
 
-    Loaded products are grouped by type, and a record is kept of which products have
-    been previously retrieved (used).
+    Loaded products are grouped by type, and retrieval of products from the depot can
+    be constrained by type, usage status or a supplied filter.
+
+    The depot keeps ProductDepot.Status usage records for all loaded products. A
+    product's status is automatically updated when it is selected for retrieval, but
+    clients can also manually update a given product's status, e.g. to reflect
+    successful or failed processing. A summary of the usage statuses of the depot's
+    products can be generated, e.g. for logging purposes.
 
     Attributes:
         types: A list of the types of products that have been loaded into in the depot.
+        Status: Enum used to mark the usage status of products in the depot.
     """
+
+    class Status(enum.Enum):
+        loaded = enum.auto()
+        retrieved = enum.auto()
+        processed = enum.auto()
+        failed = enum.auto()
+        rejected = enum.auto()
+
+        def __str__(self):
+            return self.name
 
     def __init__(self):
         self._log = logging.getLogger(__name__)
         self._products: Dict[str, List[DataProduct]] = defaultdict(list)
-        self._used: Dict[str, List[bool]] = defaultdict(list)
+        self._usage: Dict[str, Dict[str, ProductDepot.Status]] = defaultdict(dict)
 
     @property
     def types(self) -> List[str]:
@@ -30,27 +48,31 @@ class ProductDepot:
         self,
         directory: Union[Path, List[Path]],
         recursive: bool = True,
-        reject_duplicates: bool = True,
     ) -> int:
         """Load all valid PDS4 products from one or more directories.
 
-        A product is considered valid if its label declares an `msn:product_type_name`
-        supported by a registered `DataProduct` subclass.
+        A product is considered valid if its label declares a type - via
+        `msn:product_type_name` - which is supported by a registered `DataProduct`
+        subclass.
+
+        If a product is found to be identical to one already loaded into the depot, it
+        will be rejected. A given product type's notion of equality can be overridden by
+        its `DataProduct` subclass, but it is generally assumed to at least involve a
+        LID comparison.
 
         Args:
             directory: Path or list of paths to directories in which to search for
                 products. Symbolic links are followed.
             recursive: Search for products recursively.
-            reject_duplicates: Omit products that are already in the depot.
 
         Returns:
-            The number of successfully loaded products across all types.
+            The number of products successfully loaded from `directory`.
         """
         if not isinstance(directory, list):
             directory = [directory]
         num_loaded = 0
         products = self._products
-        used = self._used
+        usage = self._usage
         for dir_ in directory:
             self._log.debug(f"Loading products from '{dir_.name}'")
             glob = dir_.expanduser().rglob if recursive else dir_.expanduser().glob
@@ -62,45 +84,33 @@ class ProductDepot:
                     continue
                 type_ = product.type
                 if (
-                    reject_duplicates
-                    and type_ in products
-                    and product in products[type_]
+                    type_ in products
+                    and product in products[type_]  # assume DP sub impl sensible eq
                 ):
                     self._log.warning(f"'{product.meta.lid}' already loaded; ignoring")
                     continue
                 products[type_].append(product)
-                used[type_].append(False)
+                usage[type_][product.meta.lid] = self.Status.loaded
                 num_loaded += 1
         for type_ in products:
-            # keep products sorted by their type's defined order (e.g., acq. time)
-            # co-sort the maps to preserve correspondence across multiple loads
-            products[type_], used[type_] = (  # type: ignore
-                list(t)
-                for t in zip(
-                    *sorted(
-                        zip(products[type_], used[type_]),
-                        key=lambda v: v[0],
-                    )
-                )
-            )
+            products[type_].sort()
         return num_loaded
 
     def retrieve(
         self,
         type_: Optional[str] = None,
         filter_: Optional[Callable[[DataProduct], bool]] = None,
-        mark_used: bool = True,
-        unused: bool = True,
-        used: bool = True,
+        usage_status: Optional[Union[Status, Sequence[Optional[Status]]]] = None,
     ) -> Union[List[DataProduct], Dict[str, List[DataProduct]]]:
-        """Return all products in the depot, optionally of a given type or usage status.
+        """Retrieve products from the depot, optionally by type, usage status or filtered.
+
+        Returned products with usage Status.loaded will automatically have this updated
+        to Status.retrieved.
 
         Args:
-            type_: Restrict the retrieval to products of a given type.
-            filter_: Only consider products for which filter_(product) is True.
-            mark_used: Record retrieved products as used.
-            unused: Consider previously unused products for retrieval.
-            used: Consider previously used products for retrieval.
+            type_: Restrict the retrieval to products of a given type name.
+            filter_: Only consider products for which filter_(product, usage) is True.
+            usage_status: Only consider products with a given Usage status or statuses.
 
         Returns:
             A sorted list of retrieved products if `type_` is given, else a dictionary
@@ -108,69 +118,24 @@ class ProductDepot:
 
         Raises:
             KeyError: If the depot has never been loaded with products of `type_`.
-            ValueError: If both `unused` and `used` are False (no products to retrieve).
         """
-        if not unused and not used:
-            raise ValueError("Cannot simultaneously omit both unused and used products")
         if type_ is not None:
             self._ensure_loaded(type_)
+        if usage_status is not None and not isinstance(usage_status, Sequence):
+            usage_status = [usage_status]
         selection = defaultdict(list)
         for prod_type in [type_] if type_ is not None else self.types:
-            for prod_idx, prod_used in enumerate(self._used[prod_type]):
-                if (not prod_used and not unused) or (prod_used and not used):
+            for prod in self._products[prod_type]:
+                prod_lid = prod.meta.lid
+                prod_status = self._usage[prod_type][prod_lid]
+                if usage_status is not None and prod_status not in usage_status:
                     continue
-                prod = self._products[prod_type][prod_idx]
                 if filter_ is not None and not filter_(prod):
                     continue
+                if prod_status == self.Status.loaded:
+                    self._usage[prod_type][prod_lid] = self.Status.retrieved
                 selection[prod_type].append(prod)
-                if not prod_used and mark_used:
-                    self._used[prod_type][prod_idx] = True
         return selection if type_ is None else selection[type_]
-
-    def count(self, type_: str, unused: bool = True, used: bool = True) -> int:
-        """Return the number of products in the depot of a given type and usage status.
-
-        Args:
-            type_: Product type to count instances of.
-            unused: Count products marked as unused.
-            used: Count products marked as used.
-
-        Returns:
-            The number of `type_` products matching the usage criterium.
-
-        Raises:
-            KeyError: If the depot has never been loaded with products of `type_`.
-            ValueError: If both `unused` and `used` are False (no products to count).
-        """
-        self._ensure_loaded(type_)
-        if not unused and not used:
-            raise ValueError("Cannot simultaneously omit both unused and used products")
-        if unused and used:
-            return len(self._used[type_])
-        return self._used[type_].count(used)  # left w/XOR, so !used implies unused
-
-    def next(self, type_: str) -> Optional[DataProduct]:
-        """Return the next unused product belonging to `type_`, and record it as used.
-
-        Retrieval order is dictated by the class associated with `type_`; see
-        `DataProduct` for more information.
-
-        Args:
-            type_: Product type to retrieve.
-
-        Returns:
-            The next unused product of `type_` if one remains, else None.
-
-        Raises:
-            KeyError: If the depot has never been loaded with products of `type_`.
-        """
-        self._ensure_loaded(type_)
-        try:
-            idx = self._used[type_].index(False)
-        except ValueError:
-            return None
-        self._used[type_][idx] = True
-        return self._products[type_][idx]
 
     def match(self, type_: str, product: DataProduct) -> Optional[DataProduct]:
         """Find and return a product of `type_` which is a match for `product`.
@@ -178,9 +143,8 @@ class ProductDepot:
         Search order and matching criteria are dictated by the class associated with
         `type_`; see `DataProduct` for more information.
 
-        If a match is found, it will be marked as used, however, the usage status of
-        prospective matches is not considered (i.e., repeat calls will yield the same
-        match).
+        Matched products with usage Status.loaded will automatically have this updated
+        to Status.retrieved.
 
         Args:
             type_: Product type to consider when searching for a match.
@@ -194,16 +158,47 @@ class ProductDepot:
             KeyError: If the depot has never been loaded with products of `type_`.
         """
         self._ensure_loaded(type_)
-        for idx, candidate in enumerate(self._products[type_]):
+        for candidate in self._products[type_]:
             if candidate.matches(product):
-                break
-        else:
-            return None
-        self._used[type_][idx] = True
-        return candidate
+                lid = candidate.meta.lid
+                if self._usage[type_][lid] == self.Status.loaded:
+                    self._usage[type_][lid] = self.Status.retrieved
+                return candidate
+        return None
+
+    def mark(self, product: DataProduct, usage_status: Status) -> bool:
+        """Assign a `usage_status` to a `product`.
+
+        Args:
+            product: The DataProduct instance which should be marked.
+            usage_status: The usage Status which should be assigned to `product`.
+
+        Returns:
+            True if `product` is found and marked, else False.
+
+        Raises:
+            KeyError: If the depot has never been loaded with products of `product.type`.
+            TypeError: In the event that `usage_status` is not a member of Status.
+        """
+        type_ = product.type
+        self._ensure_loaded(type_)
+        if not isinstance(usage_status, ProductDepot.Status):
+            raise TypeError(
+                f"usage status '{usage_status}' is not a member of ProductDepot.Status"
+            )
+        lid = product.meta.lid
+        if lid not in self._usage[type_]:
+            return False
+        self._usage[type_][lid] = usage_status
+        return True
 
     def release(self, product: DataProduct) -> bool:
-        """Remove (any references to) `product` from the depot.
+        """Remove direct references to `product` from the depot.
+
+        Note that while this method - intended to allow clients to manage space
+        complexity - deletes the depot's reference to the `product` object itself, its
+        associated usage status is still preserved (as this is an indirect reference
+        mapped by LID).
 
         Args:
             product: The `DataProduct` instance which should be released.
@@ -217,12 +212,61 @@ class ProductDepot:
         type_ = product.type
         self._ensure_loaded(type_)
         try:
-            idx = self._products[type_].index(product)
+            self._products[type_].remove(product)
         except ValueError:
             return False
-        self._products[type_].pop(idx)
-        self._used[type_].pop(idx)
         return True
+
+    def count(
+        self,
+        type_: str,
+        usage_status: Optional[Status] = None,
+        ignore_released: bool = False,
+    ) -> int:
+        """Return the number of products in the depot of a given type and usage status.
+
+        Args:
+            type_: The product type to count instances of.
+            usage_status: Only count products of a given Status.
+            ignore_released: Don't count products that have been released from the depot.
+
+        Returns:
+            The number of `type_` products matching the `usage_status`.
+
+        Raises:
+            KeyError: If the depot has never been loaded with products of `type_`.
+        """
+        self._ensure_loaded(type_)
+        prods = self._products[type_]
+        usage = self._usage[type_]
+        if usage_status is None:
+            return len(prods) if ignore_released else len(usage)
+        if ignore_released:
+            return len([p for p in prods if usage[p.meta.lid] == usage_status])
+        return len([s for s in usage.values() if s == usage_status])
+
+    def usage_summary(
+        self, type_: Optional[str] = None
+    ) -> Union[Dict[Status, Sequence[str]], Dict[str, Dict[Status, Sequence[str]]]]:
+        """Return a summary of the usage statuses of the depot's products.
+
+        Args:
+            type_: Restrict the summary to products of a given type name.
+
+        Returns:
+            A dictionary mapping usage statuses to product LIDs if `type_` is given,
+            else a dictionary mapping the name of each loaded type to its status map.
+
+        Raises:
+            KeyError: If the depot has never been loaded with products of `type_`.
+        """
+        if type_ is not None:
+            self._ensure_loaded(type_)
+        summary = defaultdict(lambda: defaultdict(list))
+        for prod_type in [type_] if type_ is not None else self.types:
+            for prod_lid, prod_status in self._usage[prod_type].items():
+                summary[prod_type][prod_status].append(prod_lid)
+        return summary if type_ is None else summary[type_]  # type: ignore
 
     def _ensure_loaded(self, type_: str):
         if type_ not in self._products:
