81fbb9af7924c1e6ec5f22f86e2f48f5741d16b8
Alun Jones <alun.git@ty-penguin.org.uk>
2025-08-29 09:11:40 +0100

    Tidy up proctools/__init__.py

--- src/proctools/__init__.py
+++ src/proctools/__init__.py
@@ -1,15 +1,18 @@
-"""ProcTools - Common tools for (ExoMars) data product processing software
+"""ProcTools - Common tools for (ExoMars) data product processing software.
 
 """
+
+# Bring in some metadata from the package.
 try:
     import importlib.metadata as importlib_metadata
 except ImportError:
     import importlib_metadata
-_dist_meta = importlib_metadata.metadata("passthrough")
-__author__ = _dist_meta["Author"]
+
+# Copy it into module-level variables.
+_dist_meta = importlib_metadata.metadata("proctools")
+__author__ = _dist_meta["Author-email"]
 __description__ = _dist_meta["Summary"]
 __project__ = _dist_meta["Name"]
-__url__ = _dist_meta["Home-Page"]
 __version__ = _dist_meta["Version"]
 del _dist_meta
 
