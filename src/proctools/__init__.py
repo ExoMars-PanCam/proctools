434339b37b93832ba6d7fb7b97d14a747f8a643f
Alun Jones <alj118@aber.ac.uk>
2026-04-23 10:33:29 +0100

    Change how __url__ is set in __init__.py so it handles the newer Project-URL metadata standard.

--- src/proctools/__init__.py
+++ src/proctools/__init__.py
@@ -9,6 +9,10 @@ _dist_meta = importlib_metadata.metadata("proctools")
 __author__ = _dist_meta["Author"]
 __description__ = _dist_meta["Summary"]
 __project__ = _dist_meta["Name"]
-__url__ = _dist_meta["Home-Page"]
+__url__ = next(
+    item.replace("Repository, ", "", 1)
+        for item in _dist_meta.get_all("Project-URL")
+            if item.startswith("Repository, ")
+)
 __version__ = _dist_meta["Version"]
 del _dist_meta, importlib_metadata
